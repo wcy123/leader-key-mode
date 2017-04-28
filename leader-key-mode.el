585237426d03a370507adb7e8c9b68f1dc3733d6
Wang Chunye <wcy123@gmail.com>
2017-04-28 11:52:34 +0800

    add leader-key-mode.el

--- leader-key-mode.el
+++ leader-key-mode.el
@@ -1,16 +1,15 @@
+(defvar leader-key-mode-keymap
+  (make-sparse-keymap))
 (define-minor-mode leader-key-mode
-  "Minor mode to support <leader> support." t)
+  "Minor mode to support <leader> support." t nil leader-key-mode-keymap)
 (defvar leader-key "\\"
   "the default leader key")
 (defvar leader-key-mode-mark-active-keymap
   (make-sparse-keymap))
-(defvar leader-key-mode-keymap (make-sparse-keymap))
-(defun leader-key-mode-create-entry-keymap (key)
-  (let ((m (make-sparse-keymap)))
-    (define-key m (kbd key) leader-key-mode-keymap)
-    m))
 (defconst leader-key-mode--emulation-mode-map-alist
   `((mark-active ,@leader-key-mode-mark-active-keymap)
-    (leader-key-mode keymap (,,@(leader-key-mode-create-entry-keymap leader-key))))
+    (leader-key-mode ,@(let ((m (make-sparse-keymap)))
+                         (define-key m (kbd leader-key) leader-key-mode-keymap)
+                         m))))
 (add-to-list 'emulation-mode-map-alists 'leader-key-mode--emulation-mode-map-alist)
 (provide 'leader-key-mode)
