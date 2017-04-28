eceb74cd96f6195aa1e32dca22a16ed9a57d419b
Wang Chunye <wcy123@gmail.com>
2017-04-28 14:23:17 +0800

    add pkg.el

---
+++ leader-key-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "leader-key-mode" "0.0" "leader-key-mode is a minor modes to mimic the leader key in VIM" 'nil :url "https://github.com/wcy123/leader-key-mode" :keywords '("extensions" "leaderkey" "minor"))
